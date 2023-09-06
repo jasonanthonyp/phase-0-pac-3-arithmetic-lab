36319cde8c90d069a93089724481211620cb498b
Jason Phillips <jasonanthonyp@gmail.com>
2023-09-05 20:38:27 -0400

    commit tree 5e38d95ec94e075327df165543933bbb9d67b931

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(n) {
+    n++;
+    return n;
+  }
+
+  function decrement(n) {
+    n--;
+    return n;
+  }
+
+  function makeInt(string) {
+    let n = parseInt(string, 10);
+    return n;
+
+  }
+
+  function preserveDecimal(string) {
+    let n = parseFloat(string, 10);
+    return n;
+    
+  }
+
+
+
